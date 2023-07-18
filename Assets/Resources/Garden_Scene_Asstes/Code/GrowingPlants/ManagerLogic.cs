c2409f5627367e12b417567584aa8b8c7298d38b
szymonpoterejkoinfe2 <sp306077@student.polsl.pl>
2023-07-18 20:07:22 +0200

    ConfirmationWindow

--- Assets/Resources/Garden_Scene_Asstes/Code/GrowingPlants/ManagerLogic.cs
+++ Assets/Resources/Garden_Scene_Asstes/Code/GrowingPlants/ManagerLogic.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 public class ManagerLogic : MonoBehaviour
 {
 
-    GameObject SoilToCheck, Camera;
+    GameObject Camera;
     float Timer = 0f;
     public float GrowTime = 10f;
-    float MaxSize = 1.1f;
     public bool HaveManager = false;
     
     // Beginning of Scailing Coroutine
     public void StartGrowing()
     {
         HaveManager = true;
-            StartCoroutine(GrowWithManager());
+        StartCoroutine(GrowWithManager());
+        
     }
 
     public void UpgradeManager()
@@ -29,7 +29,7 @@ public class ManagerLogic : MonoBehaviour
     {
         Camera = GameObject.Find("Camera");
         Vector3 StartScale = new Vector3(0f, 0f, 0f);
-        Vector3 MaxScale = new Vector3(0.21f, MaxSize, 0.21f);
+        Vector3 MaxScale = new Vector3(gameObject.GetComponent<ObjectPrice>().ValueTarget[0], gameObject.GetComponent<ObjectPrice>().ValueTarget[1], gameObject.GetComponent<ObjectPrice>().ValueTarget[2]);
 
         while (HaveManager)
         {
--- Assets/Resources/Garden_Scene_Asstes/Code/TapOnTileDetector.cs
+++ Assets/Resources/Garden_Scene_Asstes/Code/TapOnTileDetector.cs
@@ -1,29 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TapOnTileDetector : MonoBehaviour
 {
     public Rotation rotation;
+    public GameObject ConfirmationWindow;
     private GameObject Tile;
     private GameObject Bank,Cover;
     ulong Ballance, Price;
     public CameraAndTileManager CameraTileManager;
-
+    private bool NotAsking;
+    public TextMeshProUGUI PriceTxt;
     // Start is called before the first frame update
+    private void Awake()
+    {
+        ConfirmationWindow.SetActive(false);
+        NotAsking = true;
+    }
 
-   
 
     // Update is called once per frame
     void Update()
     {
         Bank = GameObject.FindGameObjectWithTag("Bank");
-        
-        
-
+ 
         Ballance = Bank.GetComponent<MoneyManager>().MoneyBallance;
 
-        if (Input.touchCount == 1)
+        if (Input.touchCount == 1 && NotAsking == true)
         {
             Touch t = Input.GetTouch(0);
             Vector3 touchPos = t.position;
@@ -37,13 +42,12 @@ public class TapOnTileDetector : MonoBehaviour
                     Cover = hit.transform.gameObject;
                     Price = Cover.GetComponent<ObjectPrice>().MyPrice;
 
-                    if (Ballance >= Price)
-                    {
-                        Destroy(Cover);
-                        Bank.GetComponent<MoneyManager>().DecrementBallance(Price);
-                    }
+                    ConfirmationWindow.SetActive(true);
+                    NotAsking = false;
+                    PriceTxt.text = Cover.GetComponent<ObjectPrice>().MyPrice.ToString();
+  
                 }
-                else if (hit.transform.name == "Soil")
+                else if (hit.transform.name == "Soil" && NotAsking == true)
                 {
                     Tile = hit.transform.gameObject;
 
@@ -60,13 +64,37 @@ public class TapOnTileDetector : MonoBehaviour
                     // Swithcing to secound camera
                     CameraTileManager.ChangeToCameraTwo();
 
-                   
-
                 }
             }
 
         }
 
+    }
+
+    //Closing QuestionWindow
+    public void CloseQuestionWindow()
+    {
+        ConfirmationWindow.SetActive(false);
+        StartCoroutine(Delay());
+
+    }
+
+    //Closing QuestionWindow with expansion
+    public void WantToExpand()
+    {
+        if (Ballance >= Price)
+        {
+            Destroy(Cover);
+            Bank.GetComponent<MoneyManager>().DecrementBallance(Price);
+            CloseQuestionWindow();
+        }
+    }
+
+    //Making Delay
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(1);
+        NotAsking = true;
     }
 
 }
